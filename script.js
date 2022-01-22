afa86c8dd8f63a205f6b2ce1de19d5fbebfe1ec0
Davi-Marques <93229005+davi-marques@users.noreply.github.com>
2022-01-22 15:07:04 -0300

    toFixed()

--- script.js
+++ script.js
@@ -12,25 +12,25 @@ function calcular(A, P) {
         let imc = P / (A^2)
         if (imc < 17) {
             res.innerHTML = 'Muito abaixo do peso <br>'
-            res.append(`IMC: ${imc}`)
+            res.append(`IMC: ${imc.toFixed(2)}`)
         } else if (imc >= 18 && imc <= 18.5) {
             res.innerHTML = 'Abaixo do peso <br>'
-            res.append(`IMC: ${imc}`)
+            res.append(`IMC: ${imc.toFixed(2)}`)
         } else if (imc >= 18.6 && imc <= 25) {
             res.innerHTML = 'Peso ideal (parabéns) <br>'
-            res.append(`IMC: ${imc}`)
+            res.append(`IMC: ${imc.toFixed(2)}`)
         } else if (imc >= 25.1 && imc <= 29) {
             res.innerHTML = 'Levemente acima do peso <br>'
-            res.append(`IMC: ${imc}`)
+            res.append(`IMC: ${imc.toFixed(2)}`)
         } else if (imc >= 30 && imc <= 34.9) {
             res.innerHTML = 'Obesidade <br>'
-            res.append(`IMC: ${imc}`)
+            res.append(`IMC: ${imc.toFixed(2)}`)
         } else if (imc >= 35 && imc <= 39.9) {
             res.innerHTML = 'Obesidade severa <br>'
-            res.append(`IMC: ${imc}`)
+            res.append(`IMC: ${imc.toFixed(2)}`)
         } else {
             res.innerHTML = 'Obesidade mórbida <br>'
-            res.append(`IMC: ${imc}`)
+            res.append(`IMC: ${imc.toFixed(2)}`)
         }
     }
     calcular(Number(alt.value), Number(peso.value))
